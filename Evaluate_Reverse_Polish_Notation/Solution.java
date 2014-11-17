7b8c380d5525a793b11f95fdf005fa177e0a3904
Zhangyi Jin <billjzy637@gmail.com>
2014-11-17 16:00:31 -0500

    ERP

---
+++ Evaluate_Reverse_Polish_Notation/Solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Solution{
+   public static int evalRPN(String [] tokens){
+      if (tokens.length==0) return 0;
+      Stack<Integer> token=new Stack<Integer>();// token store the int
+      for (int i=0;i<tokens.length;i++){
+         String temp=tokens[i];
+         if (!temp.equals("+")&&!temp.equals("-")&&!temp.equals("*")&&!temp.equals("/")){
+            token.push(Integer.parseInt(temp));
+         }
+         
+         else if (!token.isEmpty()){//make sure Stack is not empty
+            if (temp=="+") {// deal with each operand 
+              int a=token.pop();
+              int b=token.pop();
+              token.push(a+b);
+            }   
+            if (temp=="-"){
+              int b=token.pop();
+              int a=token.pop();
+              token.push(a-b);
+            }
+            if (temp=="*"){
+              int a=token.pop();
+              int b=token.pop();
+              token.push(a*b);
+            }
+            if (temp=="/"){
+              int b=token.pop();
+              int a=token.pop();
+              token.push(a/b);
+            }//end with operands
+         }
+         
+      }
+     int result=token.pop();
+     return result;
+   }
+} 
+   
+
+
+
