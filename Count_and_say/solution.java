eb77ab501a22d76d5b03b91dc55586ad5099c528
Zhangyi Jin <billjzy637@gmail.com>
2014-11-17 14:01:15 -0500

    Count and say

---
+++ Count_and_say/solution.java
@@ -0,0 +1,29 @@
+public class solution{
+  public String countAndSay(int n){
+    if (n<=0){
+      return "";
+    }
+    String current="1";
+    int start=1;
+
+    while (start<n){//from string 1 to string n
+       StringBuilder temp=new StringBuilder();
+       int count=1;
+       for (int i=1;i<current.length();i++){//scan from the beginning of stringN
+           if (current.charAt(i)==current.charAt(i-1)) {
+             count++;
+           }
+           else {
+             temp.append(count);//add count 
+             temp.append(current.charAt(i-1));//add number
+             count=1;//reset count to 1
+           }
+       }//if last consective chars are same
+       temp.append(count);
+       temp.append(current.charAt(current.length()-1));
+       current=new String(temp);
+       start++;
+    }
+    return current;
+  }
+}
