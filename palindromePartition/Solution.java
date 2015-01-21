56f332ebe2ba0b4e454fe47314b7e0ee505bf811
Zhangyi Jin <billjzy637@gmail.com>
2015-01-21 10:18:30 -0500

    2nd round

--- palindromePartition/Solution.java
+++ palindromePartition/Solution.java
@@ -2,29 +2,27 @@
  * */
 import java.util.*;
 public class Solution{
-  public ArrayList<ArrayList<String>> partition(String s){
-   ArrayList<ArrayList<String>> result  = new ArrayList<ArrayList<String>>();
+  public List<List<String>> partition(String s){
+ List<List<String>> result  = new ArrayList<>();
     if (s==null ||s.length()==0) return result;
     dfs(s,0, result , new ArrayList<String>());
     return result;
   }
   //start is the break point for dfs, probe until palindrome is found
-  private void dfs(String s,  int start, ArrayList<ArrayList<String>> res, ArrayList<String> list){
+  private void dfs(String s,  int start, List<List<String>> res, List<String> list){
     if (start == s.length()){
       res.add(new ArrayList<String>(list));
       return ;
     }
-    for (int i=start+1;i<=s.length();i++){
-      String sub = s.substring(start, i);
-      if(isPalindrome(sub)){
-        list.add(sub);
+    for (int i=start;i<s.length();i++){
+      if(isPalindrome(s,start,i)){
+        list.add(s.substring(start,i+1));
         dfs(s, i, res, list);
         list.remove(list.size()-1);
       }
     }
   }
-  private boolean isPalindrome(String s){
-    int i=0,j=s.length()-1;
+  private boolean isPalindrome(String s,int i,int j){
     while (i<j){
       if (s.charAt(i)!=s.charAt(j)) return false;
       i++;
